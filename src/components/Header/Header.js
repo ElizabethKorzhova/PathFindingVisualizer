111da849d9a91442af3a0272aca32d644cdb85ce
HotSweetie <elizabeth.korzhova@gmail.com>
2021-11-05 00:18:19 +0200

    translated to eng

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,7 @@ import styles from "./Header.module.css";
 
 const Header = () => (
     <div className={styles.container}>
-        <p className={styles.caps}>платформа візуалізації алгоритмів пошуку шляху на площині</p>
-        <p>Курсова робота Коржової Єлизавети Валеріївни</p>
+        <p className={styles.caps}>Path Finding Visualizer</p>
     </div>
 );
 
